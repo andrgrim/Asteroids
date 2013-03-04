b9653744a2c93b430bd53e402fa09097a91b6546
Andre Grima <andregrima@hotmail.com>
2013-03-04 11:28:52 +0100

    Asteroid Prefabs changing driection

--- AsteroidsGame/Assets/AsteroidController.js
+++ AsteroidsGame/Assets/AsteroidController.js
@@ -1,11 +1,43 @@
 #pragma strict
 
+var direction:int;
+
+
+
 function Start () {
 
+	direction = Mathf.Round(Random.Range(1,2));
+	print(direction);
+	
+	
+
 }
+
+
 
 function Update () {
 
 	transform.Rotate(Vector3.forward*50*Time.deltaTime);
 
+	if (direction == 1){
+		
+		transform.Translate(Vector3.left*10*Time.deltaTime,Space.World);
+	}
+	if (direction == 2){
+		
+		transform.Translate(Vector3.right*10*Time.deltaTime,Space.World);
+	}
+	
+	if(transform.position.x > BorderScript.rightmost){
+		Destroy(this.gameObject);
+	}	
+	if(transform.position.x < BorderScript.leftmost){
+		Destroy(this.gameObject);
+	}		
+	if(transform.position.y < BorderScript.bottommost){
+		Destroy(this.gameObject);
+	}
+	if(transform.position.y > BorderScript.topmost){
+		Destroy(this.gameObject);
+	}
 }
---
+++ AsteroidsGame/Assets/AsteroidGenerator.js
@@ -0,0 +1,31 @@
+#pragma strict
+
+var asteroidPrefab:Rigidbody;
+var maxAsteroids:int;
+
+function createAsteroids () {
+	if (GameObject.FindGameObjectsWithTag("Asteroid").Length < 5)
+	{
+	
+	var x:float;
+	var y:float;
+	
+	x = Random.Range(-10,10);
+	y = Random.Range(-10,10);
+	
+	//create a fish instance
+	
+	Instantiate(asteroidPrefab,Vector3(x,y,2),Quaternion.identity);
+	}
+}
+
+
+function Start () {
+
+	InvokeRepeating("createAsteroids",0,2.0);
+
+}
+
+function Update () {
+
+}
